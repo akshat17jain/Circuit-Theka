07f5edef02f0b5c35a37778da7221d535945a318
SHUHBHAM KUMAR <88201167+shubham3279@users.noreply.github.com>
2022-12-14 20:28:50 +0530

    Delete ee directory

--- ee/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserImage(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, )
-    img = models.ImageField(upload_to='userimg/', blank=True)
-
-    def __str__(self): 
-        return self.author
