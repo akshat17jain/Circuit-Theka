02ddbe43b88481946e9503d494b71ae59f857206
Akshat Jain <34414705+akshat17jain@users.noreply.github.com>
2022-12-22 02:33:43 +0530

    global components_final

--- ElectronicEye.py
+++ ElectronicEye.py
@@ -120,7 +120,7 @@ def ElelctronicEye(Img):
             if(result[0]==8):
                 components.append('Transistor')
 
-        components_final = components
+        global components_final = components
     
     return components_final
         
